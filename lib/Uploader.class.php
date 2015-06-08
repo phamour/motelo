b295747f01341ee4ff5d9e3ff82b3651d5681871
yipeng <huang.ypeng@gmail.com>
2015-06-08 22:51:10 +0200

    unify upload file ext

--- lib/Uploader.class.php
+++ lib/Uploader.class.php
@@ -9,6 +9,22 @@ class Uploader {
 
         $path = STORAGE_DIR . $type . '/' . $file[$name]['name'];
 
+        // unify ext
+        $ext = '.txt';
+        switch ($type) {
+            default:
+            case TYPE_SOLUTION:
+                $ext = '.sol';
+                break;
+            case TYPE_INSTANCE:
+                $ext = '.dat';
+                break;
+            case TYPE_MODEL:
+                $ext = '.mod';
+                break;
+        }
+        $path = str_replace('.txt', $ext, $path);
+
         return !file_exists($path) && move_uploaded_file($file[$name]['tmp_name'], $path);
     }
 
