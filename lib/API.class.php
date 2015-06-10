76496f2483dc509f9853369670182cc580cef82d
yipeng <huang.ypeng@gmail.com>
2015-06-10 20:48:04 +0200

    refactory API, routes and renders
    1. move parsers into API class
    2. gather renders with templates in Utils class
    3. change routes render logic

--- lib/API.class.php
+++ lib/API.class.php
@@ -42,6 +42,125 @@ class API {
         return $results;
     }
 
+    public static function getFileContent($type, $filename) {
+        $ext = '.txt';
+        switch ($type) {
+            default:
+            case TYPE_SOLUTION:
+                $ext = '.sol';
+                break;
+            case TYPE_INSTANCE:
+                $ext = '.dat';
+                break;
+            case TYPE_MODEL:
+                $ext = '.mod';
+                break;
+        }
+        $path = STORAGE_DIR . $type . '/' . $filename . $ext;
+        return file_get_contents($path);
+    }
+
+    private static function removeComments($source) {
+        if (!$source) {
+            return '{}';
+        }
+
+        // replace block comments
+        $source = preg_replace('/(\/\*((([^*]+)|\*(?!\/))*)\*\/)/', '', $source);
+
+        // replace inline comments
+        $source = preg_replace('/(\/\/.*[\n\r])/', '', $source);
+
+        return $source;
+    }
+
+    public static function dat2json($source) {
+        if (!$source) {
+            return '{}';
+        }
+
+        // remove comments
+        $source = self::removeComments($source);
+
+        // keep the content in form
+        $content = $source;
+
+        // strip white-spaces and line-ends
+        $source = preg_replace('/[\s\n\r]+/', '', $source);
+
+        // format to json key-value pairs
+        $source = preg_replace('/([^=]+)=([^;]+);/', '"${1}":$2,', $source);
+
+        // remove last comma
+        if (substr($source, -1) == ',') {
+            $source = substr($source, 0, strlen($source) - 1);
+        }
+
+        // parse arcs
+        $source = preg_replace('/"arcs":{([<>0-9,]+)}/', '"arcs":[${1}]', $source);
+        $source = preg_replace('/<([0-9]+,[0-9]+,[0-9.]+)>/', '[${1}]', $source);
+
+        $data = json_decode('{' . $source . '}', true);
+        $data['content'] = $content;
+
+        // encap to json object
+        return json_encode($data);
+    }
+
+    public static function sol2json($source) {
+        if (!$source) {
+            return '{}';
+        }
+
+        // remove comments
+        $source = self::removeComments($source);
+
+        // parse obj value and time
+        $matches = array();
+        preg_match('/z\s*=\s*(-?[0-9.]+)/', $source, $matches);
+        $z = intval($matches[1]);
+        preg_match('/time\s*=\s*([0-9.]+)/', $source, $matches);
+        $t = floatval($matches[1]);
+        
+        return json_encode(array(
+            'z'       => $z,
+            't'       => $t,
+            'f'       => self::parseSol($source, 'f', 4),
+            'y'       => self::parseSol($source, 'y', 4),
+            'x'       => self::parseSol($source, 'x'),
+            'content' => $source
+        ));
+    }
+
+    private static function parseSol($source, $key, $dimension = 2) {
+        // construct RegEx pattern
+        $pattern = '/' . $key;
+        for ($i = 0; $i < $dimension; $i++) {
+            $pattern .= '\[([0-9]+)\]';
+        }
+        $pattern .= '\s*=\s*(-?[0-9]+)/';
+
+        // matches holder
+        $matches = array();
+        // result holder
+        $result = array();
+
+        // match
+        preg_match_all($pattern, $source, $matches);
+
+        // parse match result
+        foreach ($matches[0] as $j => $value) {
+            if ($matches[$dimension + 1][$j] != '0') {
+                $tmp = array();
+                for ($i = 0; $i <= $dimension; $i++) {
+                    $tmp[] = intval($matches[$i + 1][$j]);
+                }
+                $result[] = $tmp;
+            }
+        }
+        return $result;
+    }
+
 }
 
 // END /lib/API.class.php
--- public/index.php
+++ public/index.php
@@ -28,110 +28,72 @@ $app->get('/', function() use ($app) {
 });
 
 $app->get('/list/:type', function($type) use ($app) {
-    $app->render('header.php');
-    $app->render('flash.php');
-    switch ($type) {
-        default:
-        case TYPE_SOLUTION:
-            // complex call with joins on model and instance
-            $data = API::getList($app->db, 'solution AS s INNER JOIN model as m, instance as i', array(
-                'fields' => array(
-                    's.*',
-                    'm.label as model_label',
-                    'i.label as instance_label'
-                ),
-                'where' => 's.model_id=m.id and s.instance_id=i.id and s.status=1',
-                'json' => false
-            ));
-            $app->render('list_solution.php', compact('app', 'data'));
-            break;
-        case TYPE_INSTANCE:
-            // simple call with all cols
-            $data = API::getList($app->db, 'instance', array('json' => false));
-            $app->render('list_instance.php', compact('app', 'data'));
-            break;
-        case TYPE_MODEL:
-            // simple call with all cols
-            $data = API::getList($app->db, 'model', array('json' => false));
-            $app->render('list_model.php', compact('app', 'data'));
-            break;
+    $data = array();
+    if ($type === TYPE_SOLUTION) {
+        // complex call with joins on model and instance
+        $data = API::getList($app->db, 'solution AS s INNER JOIN model as m, instance as i', array(
+            'fields' => array(
+                's.*',
+                'm.label as model_label',
+                'i.label as instance_label'
+            ),
+            'where' => 's.model_id=m.id and s.instance_id=i.id and s.status=1',
+            'json' => false
+        ));
+    } else {
+        // simple call with all cols
+        $data = API::getList($app->db, $type, array('json' => false));
     }
-    $app->render('jsincludes.php', array(
-        'scripts' => array('/js/list.js')
-    ));
-    $app->render('footer.php');
+
+    $body = 'list_' . $type . '.php';
+    $bodyParams = compact('app', 'data');
+    $jsParams = array('scripts' => array('/js/list.js'));
+
+    Utils::renderLayout($app, $body, $bodyParams, $jsParams);
 })->name('list');
 
 $app->get('/create/:type', function($type) use ($app) {
-    $app->render('header.php');
-    $app->render('flash.php');
-    switch ($type) {
-        default:
-        case TYPE_SOLUTION:
-            $app->render('creation_solution.php');
-            break;
-        case TYPE_INSTANCE:
-            $app->render('creation_instance.php');
-            break;
-        case TYPE_MODEL:
-            $app->render('creation_model.php');
-            break;
-    }
-    $app->render('jsincludes.php', array(
-        'scripts' => array('/js/form.js')
-    ));
-    $app->render('footer.php');
+    $body = 'creation_' . $type . '.php';
+    $bodyParams = array();
+    $jsParams = array('scripts' => array('/js/form.js'));
+
+    Utils::renderLayout($app, $body, $bodyParams, $jsParams);
 });
 
 $app->post('/create/:type', function($type) use ($app) {
     FormActions::create($app, $type);
-    $app->redirect('/create/' . $type);
+    $app->redirectTo('list', compact('type'));
 });
 
 $app->get('/edit/:type/:id', function($type, $id) use ($app) {
     //var_dump($app->request);die;
-    $data = CRUD::raw($app->db, array(
-        'table' => $type,
-        'where' => 'id=' . $id
+    $data = API::getOne($app->db, $type, array(
+        'where' => 'id=' . $id,
+        'json'  => false
     ));
-    $app->render('header.php');
-    $app->render('flash.php');
     if (!$data) {
         Utils::yflash('error', 'Failed to load the data for edit.');
         $app->redirect('/');
-    } else {
-        switch ($type) {
-            default:
-            case TYPE_SOLUTION:
-                $app->render('creation_solution.php', compact('data'));
-                break;
-            case TYPE_INSTANCE:
-                $app->render('creation_instance.php', compact('data'));
-                break;
-            case TYPE_MODEL:
-                $app->render('creation_model.php', compact('data'));
-                break;
-        }
     }
-    $app->render('jsincludes.php', array(
-        'scripts' => array('/js/form.js')
-    ));
-    $app->render('footer.php');
+
+    $body = 'creation_' . $type . '.php';
+    $bodyParams = compact('data');
+    $jsParams = array('scripts' => array('/js/form.js'));
+
+    Utils::renderLayout($app, $body, $bodyParams, $jsParams);
 })->name('edit');
 
 $app->put('/edit/:type/:id', function($type, $id) use ($app) {
     FormActions::update($app, $type, $id);
-    $app->redirect('/list/' . $type);
+    $app->redirectTo('list', compact('type'));
 });
 
 $app->delete('/delete/:type/:id', function($type, $id) use ($app) {
     FormActions::delete($app, $type, $id);
-    $app->redirect('/list/' . $type);
+    $app->redirectTo('list', compact('type'));
 })->name('delete');
 
 $app->get('/view/:type/:id', function($type, $id) use ($app) {
-    $app->render('header.php');
-    $app->render('flash.php');
     switch ($type) {
         default:
         case TYPE_SOLUTION:
@@ -146,7 +108,6 @@ $app->get('/view/:type/:id', function($type, $id) use ($app) {
             ));
             $data['instance_url'] = $app->urlFor('getdat', array('filename' => $data['instance_filename']));
             $data['url'] = $app->urlFor('getsol', array('filename' => $data['filename']));
-            $app->render('view_solution.php', compact('app', 'data'));
             break;
         case TYPE_INSTANCE:
             // simple call with all cols
@@ -155,51 +116,35 @@ $app->get('/view/:type/:id', function($type, $id) use ($app) {
                 'json'  => false
             ));
             $data['url'] = $app->urlFor('getdat', array('filename' => $data['filename']));
-            $app->render('view_instance.php', compact('app', 'data'));
             break;
     }
-    $app->render('jsincludes.php', array(
+
+    $body = 'view_' . $type . '.php';
+    $bodyParams = compact('app', 'data');
+    $jsParams = array(
         'libs'    => array(
             '/js/lib/cytoscape.min.js'
         ),
         'scripts' => array(
             '/js/view.js'
         )
-    ));
-    $app->render('footer.php');
+    );
+
+    Utils::renderLayout($app, $body, $bodyParams, $jsParams);
 })->name('view');
 
 // API
-// TODO: need refactoring
 $app->get('/getcontent/:type/:filename/', function($type, $filename) use ($app) {
-    $path = STORAGE_DIR;
-    $ext = '.txt';
-    switch ($type) {
-        default:
-        case TYPE_SOLUTION:
-            $path .= 'solution/';
-            $ext = '.sol';
-            break;
-        case TYPE_INSTANCE:
-            $path .= 'instance/';
-            $ext = '.dat';
-            break;
-        case TYPE_MODEL:
-            $path .= 'model/';
-            $ext = '.mod';
-            break;
-    }
-    $path .= $filename . $ext;
-    echo file_get_contents($path);
-});
+    echo API::getFileContent($type, $filename);
+})->name('getcontent');
 
 $app->get('/getdat/:filename', function($filename) use ($app) {
-    echo Utils::dat2json(file_get_contents(STORAGE_DIR . 'instance/' . $filename . '.dat'));
+    echo API::dat2json(API::getFileContent('instance', $filename));
 })->name('getdat');
 
 $app->get('/getsol/:filename', function($filename) use ($app) {
-    echo Utils::sol2json(file_get_contents(STORAGE_DIR . 'solution/' . $filename . '.sol'));
-})->name('getsol');;
+    echo API::sol2json(API::getFileContent('solution', $filename));
+})->name('getsol');
 
 $app->get('/getmodels', function() use ($app) {
     echo API::getList($app->db, 'model');
