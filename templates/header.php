3cdcbf4c9cf1c6d4eeece6a4b7c9a44a74e28e69
yipeng <huang.ypeng@gmail.com>
2015-06-12 02:51:57 +0200

    general ui, add create directly on nav, #2

--- templates/header.php
+++ templates/header.php
@@ -23,6 +23,28 @@
             </a>
             <div class="collapse navbar-collapse" id="navigation">
                 <ul class="nav navbar-nav">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            Create <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="<?= $app->urlFor('create', array('type' => 'model')) ?>">
+                                    Create model
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= $app->urlFor('create', array('type' => 'instance')) ?>">
+                                    Create instance
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= $app->urlFor('create', array('type' => 'solution')) ?>">
+                                    Create solution
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
                     <li class="<?= preg_match('/\/model/', $app->request->getResourceUri()) ? 'active' : '' ?>">
                         <a href="<?= $app->urlFor('list', array('type' => 'model')) ?>">
                             Model
