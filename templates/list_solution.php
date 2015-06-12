60aaf484bf1f74818f40018f3d472609e38e85e4
yipeng <huang.ypeng@gmail.com>
2015-06-12 02:24:04 +0200

    list ui, add deletion confirm, #2

--- templates/list_solution.php
+++ templates/list_solution.php
@@ -50,13 +50,33 @@
                                 $app->urlFor('edit', array('type' => 'solution', 'id' => $row['id'])) ?>">
                                 <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
                             </a>
-                            <form class="list_action" method="post" action="<?= 
-                                $app->urlFor('delete', array('type' => 'solution', 'id' => $row['id'])) ?>">
-                                <input type="hidden" name="_METHOD" value="DELETE">
-                                <button class="btn btn-default" title="delete">
-                                    <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
-                                </button>
-                            </form>
+                            <a class="list_action action_edit btn btn-default" role="button" title="delete" href="#" 
+                                data-toggle="modal" data-target="#list_delete_<?= $row['id'] ?>">
+                                <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                            </a>
+                            <div class="modal fade" id="list_delete_<?= $row['id'] ?>" tabindex="-1" role="dialog" 
+                                aria-labelledby="list_delete_label_<?= $row['id'] ?>" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h4 class="modal-title" id="list_delete_label_<?= $row['id'] ?>">
+                                                Are you sure to delete the solution?
+                                            </h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            This action <strong>CANNOT</strong> be undone at the application level.
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                            <form class="list_action" method="post" action="<?= 
+                                                $app->urlFor('delete', array('type' => 'solution', 'id' => $row['id'])) ?>">
+                                                <input type="hidden" name="_METHOD" value="DELETE">
+                                                <button class="btn btn-danger" title="delete">Delete</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </td>
                     </tr>
                 <?php endforeach ?>
