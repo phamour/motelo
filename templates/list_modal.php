a33aac5a07456201c5e9bbba0812d388c02b6d1b
yipeng <huang.ypeng@gmail.com>
2015-06-13 15:44:18 +0200

    add loading image to file wrapper on lists, #15

--- templates/list_modal.php
+++ templates/list_modal.php
@@ -5,7 +5,10 @@
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title" id="file_content_modal_label">File content</h4>
             </div>
-            <div class="modal-body"><pre></pre></div>
+            <div class="modal-body">
+                <img class="loading_img center-block" src="/img/loading.gif">
+                <pre></pre>
+            </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
             </div>
