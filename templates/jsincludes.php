23ef408da66d023185ffe33aeb8e14434ca8ad9a
yipeng <huang.ypeng@gmail.com>
2015-09-26 20:56:21 +0200

    include angular, #27

--- templates/jsincludes.php
+++ templates/jsincludes.php
@@ -1,3 +1,9 @@
+<!-- Include the core AngularJS library -->
+<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.5/angular.min.js"></script>
+
+<!-- Include the AngularJS routing library -->
+<script src="https://code.angularjs.org/1.2.28/angular-route.min.js"></script>
+
 <!-- Permanent libraries -->
 <script src="/js/lib/jquery-1.11.3.min.js"></script>
 <script src="/js/lib/bootstrap.min.js"></script>
